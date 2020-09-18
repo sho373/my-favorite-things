022c45b279a736e5451d2c8c880dcbdc64bac840
sho373 <polanditblog@gmail.com>
2020-09-18 18:25:37 +0200

    fix about page

--- src/pages/sign-out/sign-out.component.jsx
+++ src/pages/sign-out/sign-out.component.jsx
@@ -3,18 +3,16 @@ import Link from '@material-ui/core/Link';
 
 import './sign-out.styles.scss';
 
-class SignOut extends React.Component {
-  render() {
-    return (
-      <div className="sign-out">
-        <h2 className="title">You have been signed out</h2>
-        <span>Thank you for using this app!</span>
-        <h4 className="sign-in-here">
-          sign in <Link href="/signin">here</Link>
-        </h4>
-      </div>
-    );
-  }
-}
+export const SignOut = () => {
+  return (
+    <div className="sign-out">
+      <h2 className="title">You have been signed out</h2>
+      <span>Thank you for using this app!</span>
+      <h4 className="sign-in-here">
+        sign in <Link href="/signin">here</Link>
+      </h4>
+    </div>
+  );
+};
 
 export default SignOut;
